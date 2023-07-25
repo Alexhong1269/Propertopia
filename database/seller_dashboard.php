7c4b42adc6cffbd55798214657adde0cdc3eb408
lbu0413 <vvookly@gmail.com>
2023-07-25 13:16:24 -0400

    work so far from branch wook

--- database/seller_dashboard.php
+++ database/seller_dashboard.php
@@ -11,8 +11,13 @@
     <?php
     session_start();
     $username = $_SESSION['first_name'];
-    echo "<h1>Welcome, {$username}!</h1>";
-    echo "<div>Seller Dashboard</div>";
+    echo "
+    <div>
+        <h1>Welcome, {$username}!</h1>
+        <div>Seller Dashboard</div>
+    </div>
+    ";
+
     ?>
 </body>
 
